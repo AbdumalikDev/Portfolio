5093753b158e8d781245a7c16e72780a495f84be
phenomenalprogrammer <hojimirzaevabdumalik@gmali.com>
2021-03-31 08:42:08 +0500

    commit on master

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,34 +12,36 @@ const Header = () => {
     <header>
       <div
         onClick={handleClick}
-        className={showMenu ? 'menu-btn show' : 'menu-btn close'}
+        className={showMenu ? 'menu-btn close' : 'menu-btn'}
       >
         <div className='btn-line'></div>
         <div className='btn-line'></div>
         <div className='btn-line'></div>
       </div>
 
-      <nav className='menu'>
-        <div className='menu-branding'>
+      <nav className={showMenu ? 'menu show' : 'menu'}>
+        <div className={showMenu ? 'menu-branding show' : 'menu-branding'}>
           <div className='portrait'></div>
         </div>
-        <ul className='menu-nav'>
-          <li className='nav-item'>
+        <ul className={showMenu ? 'menu-nav show' : 'menu-nav'}>
+          <li
+            className={showMenu ? 'nav-item current show' : 'nav-item current'}
+          >
             <Link to='/' className='nav-link'>
               Home
             </Link>
           </li>
-          <li className='nav-item'>
+          <li className={showMenu ? 'nav-item show' : 'nav-item'}>
             <Link to='/' className='nav-link'>
               My Projects
             </Link>
           </li>
-          <li className='nav-item'>
+          <li className={showMenu ? 'nav-item show' : 'nav-item'}>
             <Link to='/' className='nav-link'>
               About
             </Link>
           </li>
-          <li className='nav-item'>
+          <li className={showMenu ? 'nav-item show' : 'nav-item'}>
             <Link to='/' className='nav-link'>
               Contact Me
             </Link>
