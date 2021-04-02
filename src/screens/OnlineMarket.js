5cd1b1b7c4bacf2ebc2026107b026a3f7703b719
phenomenalprogrammer <hojimirzaevabdumalik@gmali.com>
2021-04-02 15:25:24 +0500

    commit on master

---
+++ src/screens/OnlineMarket.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const OnlineMarket = () => {
+  return (
+    <div>
+      Sorry, this website has not been hosted yet. It is being developed
+    </div>
+  );
+};
+
+export default OnlineMarket;
